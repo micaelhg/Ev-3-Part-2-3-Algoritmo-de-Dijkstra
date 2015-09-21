f2853a3f75aa9fa6c1eb2f432c20c68f2e458453
Micael <hidalgomicael@gmail.com>
2015-09-21 14:27:50 -0400

    Se agrega inicializa grafo

--- main.c
+++ main.c
@@ -8,17 +8,58 @@ typedef struct tipo_adyacentes{
 
 typedef struct tipo_nodo{     /* se define un nodo generico para las dos tipos de busquedas*/
   int ID;       /* etiqueta */
+  char nombreHosp;
   adyacentes *primerVecino;
   int padre; /* indice del padre en el arreglo de nodos  */
 }Nodo;
 
-void inicializa(char *nameHospitals;){
-    nameHospitals[0]="a";  nameHospitals[1]="b";  nameHospitals[2]="c";  nameHospitals[3]="d";
-    nameHospitals[4]="e";  nameHospitals[5]="f";   nameHospitals[6]="g";  nameHospitals[7]="h";
-    nameHospitals[8]="i";   nameHospitals[9]="j";   nameHospitals[10]="k";nameHospitals[3]="l";
+char devuelveChar(int i){
+    switch ( i )
+		{
+            case  0: return 'a';
+            case  1: return 'b';
+            case  2: return 'c';
+            case  3: return 'd';
+            case  4: return 'e';
+            case  5: return 'f';
+            case  6: return 'g';
+            case  7: return 'h';
+
+		}
 }
 
-main(){
-    char nameHospitals[11];
-    inicializa( nameHospitals );
+adyacentes devuelvePrimerVecino(int i){
+        switch ( i )
+		{
+            case  0: return (a);
+            case  1: return 'b';
+            case  2: return 'c';
+            case  3: return 'd';
+            case  4: return 'e';
+            case  5: return 'f';
+            case  6: return 'g';
+            case  7: return 'h';
+
+
+		}
+}
+
+
+int inicializaG(Nodo grafo[] ){
+    int i=0;
+    while (i<8){
+        grafo[i].ID=i;
+        grafo[i].nombreHosp=devuelveChar(i);
+       // grafo[i]->primerVecino=devuelvePrimerVecino(i);
+        i++;
+    }
+}
+
+int main(){
+    Nodo grafo[7];
+    printf("-Se Declaro Grafo \n ");
+    if (inicializaG( grafo )) {
+        printf("-Se inicializo el grafo \n");
+    }
+    return 1;
 }
