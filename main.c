3d78ef28c814aa7b400454284a7b33516ea03dda
Micael <hidalgomicael@gmail.com>
2015-09-21 16:30:00 -0400

    devuelvelistaVecions()
    se termina funcion NO ESTA COMPILABLE per como idea debiera funcionar
    una vez reparados los errores

--- main.c
+++ main.c
@@ -28,11 +28,12 @@ char devuelveChar(int i){
 
 		}
 }
-adyacentes inicializaNodo(adyacentes auxiliar, int valorIndice, int valorPeso, adyacentes dirMem){
+adyacentes inicializaNodo(adyacentes auxiliar, int valorIndice, int valorPeso){
     auxiliar=malloc(sizeof(auxiliar));
     auxiliar->indice=valorIndice;
     auxiliar->peso=valorPeso;
-    auxiliar->sgte=dirMem;
+    auxiliar->sgte=NULL;
+    return auxiliar;
 }
 
 adyacentes devuelveListaVecinos(int i){
@@ -40,19 +41,27 @@ adyacentes devuelveListaVecinos(int i){
         switch ( i )
 		{
             case  0:
-                aux=inicializaNodo(aux, 1, 1, NULL);
-                return aux;
+                aux=inicializaNodo(aux, 1, 1);
             case  1:
-                aux=inicializaNodo(aux, 2, 1, NULL);
-                aux->sgte=inicializaNodo(aux->ste, 4, 2, NULL);
-
-            case  2: return 'c';
-            case  3: return 'd';
-            case  4: return 'e';
-            case  5: return 'f';
-            case  6: return 'g';
-            case  7: return 'h';
+                aux=inicializaNodo(aux, 2, 1);
+                aux->sgte=inicializaNodo(aux->ste, 3, 2);
+            case  2:
+                aux=inicializaNodo(aux, 0, 2);
+                aux->sgte=inicializaNodo(aux->sgte, 4, 2);
+            case  3:
+                aux=inicializaNodo(aux, 2, 1);
+            case  4:
+                aux=inicializaNodo(aux, 5, 1);
+                aux->sgte=inicializaNodo(aux->sgte, 6, 2);
+            case  5:
+                aux=inicializaNodo(aux, 6, 1);
+                aux-sgte=inicializaNodo(aux->sgte, 3, 2);
+            case  6:
+                aux=inicializaNodo(aux, 7, 1);
+            case  7:
+                aux=inicializaNodo(aux, 5, 2);
 		}
+		return aux;
 }
 
 
@@ -62,6 +71,7 @@ int inicializaG(Nodo grafo[]){
         grafo[i].ID=i;
         grafo[i].nombreHosp=devuelveChar(i);
         grafo[i].primerVecino=devuelveListaVecinos(i);
+        grafo[i].padre=-1; //no hay padre
         i++;
     }
 }
